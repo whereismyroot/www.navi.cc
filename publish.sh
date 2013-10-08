71a5ed12e73aeeacae615cf08b3212ebde222329
baden <baden.i.ua@gmail.com>
2013-10-08 14:44:33 +0300

    Публикация сервера через rsync

--- publish.sh
+++ publish.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+SERVER=baden@new.navi.cc:~/SDK/newgps.navi.cc/www-$BRANCH/
+
+echo "Publich: $BRANCH"
+
 grunt build
+echo "rsync -avz --delete -e ssh ./dist/ $SERVER"
+rsync -avz --delete -e ssh ./dist/ $SERVER
 
 
 #cd ../www.navi.cc.ghpages
